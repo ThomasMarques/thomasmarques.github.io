8f9521a65b93917ba988a18d8907b714d15c4ccd
Thomas Marques <marquesthom@gmail.com>
2014-04-09 22:49:36 +0200

    CSS and JS for the menu and the welcome page.
    Signed-off-by: Thomas Marques <marquesthom@gmail.com>

---
+++ resources/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".hire-me").click(function () {
+        return $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 500), !1
+    }), $("ul.nav-pills li a").click(function () {
+        $("ul.nav-pills li.active").removeClass("active"), $(this).parent("li").addClass("active")
+    }), $("#main-menu").onePageNav({
+        currentClass: "active",
+        changeHash: !1,
+        scrollThreshold: .5,
+        scrollSpeed: 750,
+        filter: "",
+        easing: "swing"
+    })
+});
