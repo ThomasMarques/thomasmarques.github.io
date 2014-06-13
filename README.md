af2e6bed880f35322bff58ac5e2eac60fa68941e
Thomas Marques <marquesthom@gmail.com>
2014-06-13 15:15:54 +0200

    Minify and CDN.
    Signed-off-by: Thomas Marques <marquesthom@gmail.com>

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ thomasmarques.github.io
 ============
 
 link : http://thomasmarques.github.io/
+
+Tools
+=========
+
+- Bootstrap
+- JQuery
+- JQuery.ScrollTo
+- JQuery-One-Page-Nav
+- Font-Awesome
+- Magnific-Popup
---
+++ resources-dev/i18n/i18n.js
@@ -0,0 +1,76 @@
+/*
+ * My own i18n with javascript
+ */
+function createCookie(e, t, n) {
+    var r;
+    if (n) {
+        var i = new Date;
+        i.setTime(i.getTime() + n * 24 * 60 * 60 * 1e3);
+        r = "; expires=" + i.toGMTString()
+    } else {
+        r = ""
+    }
+    document.cookie = e + "=" + t + r + "; path=/"
+}
+
+function getCookie(e) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(e + "=");
+        if (c_start != -1) {
+            c_start = c_start + e.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length
+            }
+            return unescape(document.cookie.substring(c_start, c_end))
+        }
+    }
+    return undefined
+}
+
+function changeForLater() {
+    if (value == "en") {
+        $(".french-flag").addClass("off");
+        $(".english-flag").removeClass("off")
+    } else {
+        $(".french-flag").removeClass("off");
+        $(".english-flag").addClass("off")
+    }
+    $(".i18n").each(function () {
+        $(this).html(json_data[value][this.getAttribute("i18n-key")])
+    })
+}
+
+function changeLang(e, t) {
+    if (value == e) return;
+    value = e;
+    createCookie("lang", value, 7);
+    if (t) {
+        $(".to-fade").removeClass("fadein").addClass("fadeout");
+        $(".i18n").removeClass("fadein").addClass("fadeout");
+        setTimeout(changeForLater, 800);
+        setTimeout(function () {
+            $(".to-fade").addClass("fadein").removeClass("fadeout");
+            $(".i18n").addClass("fadein").removeClass("fadeout")
+        }, 1e3)
+    } else {
+        changeForLater()
+    }
+}
+var value;
+var json_data = new Object;
+$(document).ready(function () {
+    {
+        $.getJSON("resources-dev/i18n/i18n-data.json", function (e) {
+            e.forEach(function (e) {
+                json_data[e.lang] = e
+            });
+            var t = getCookie("lang");
+            if (t == undefined) t = "en";
+            changeLang(t, false)
+        })
+    }
+    $(".i18n-language-selector").click(function (e) {
+        changeLang(this.getAttribute("value"), true)
+    })
+})
